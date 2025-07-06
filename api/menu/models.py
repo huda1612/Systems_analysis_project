7cfa706426f79affda120e09e224e2fdda0b9499
huda <dodezhh@hotmail.com>
2025-07-06 18:38:04 +0300

    Initial commit by huda : Add Django project inside 'api' folder with the code of the model

---
+++ api/menu/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class MenuItem(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return self.name
